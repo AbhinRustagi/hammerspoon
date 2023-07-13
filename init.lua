86818feadc9ce37cf2d7fc285c9909ae72ac0728
AbhinRustagi <rustagi.abhin@gmail.com>
2023-07-13 12:08:55 +0530

    feat: add spoons

--- init.lua
+++ init.lua
@@ -4,28 +4,18 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
 
--- Wifi Changes
+-- Spoons
 
-wifiWatcher = nil
-lastSSID = hs.wifi.currentNetwork()
+hs.loadSpoon("WifiNotifier")
+spoon.WifiNotifier:init()
+spoon.WifiNotifier:start()
 
-function ssidChangedCallback()
-    newSSID = hs.wifi.currentNetwork()
+mapping = {
+  f = "Firefox",
+  t = "Terminal",
+  v = "Sublime Text",
+  w = "WhatsApp"
+}
 
-    if newSSID == nil then
-      hs.notify.new({
-          title = string.format("Disconnected from Wifi", newSSID),
-          informativeText = "Connection lost"
-        }):send()
-    elseif newSSID ~= lastSSID and newSSID ~= nil then
-      hs.notify.new({
-        title = string.format("Connected to %s", newSSID),
-        informativeText = "Wifi Network was changed"
-      }):send()
-    end
-      
-    lastSSID = newSSID
-end
-
-wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
-wifiWatcher:start()
+hs.loadSpoon("AppLauncher")
+spoon.AppLauncher:bindHotkeys(mapping)
